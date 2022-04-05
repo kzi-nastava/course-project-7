a614bfea99df793d92d5ef765fedfcb985542ab8
magley <60552482+magley@users.noreply.github.com>
2022-04-05 16:01:11 +0200

    Create Visual Studio solution
    Create initial VS solution for the project
    
    Built under Visual Studio 2019 on .NET Core 3.1.
    The Newtonsoft package is included for JSON manipulation.

---
+++ Hospital_Information_System/Hospital_Information_System/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Hospital_Information_System
+{
+    class Person
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public List<int> FavNums { get; set; }
+        public override String ToString()
+        {
+            return "Person(" + Name + ", " + Age + ", [" + string.Join(", ", FavNums) + "]" + ")";
+        }
+        public Person()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const String dataFolder = "../../../data/";
+
+            String jsonText = System.IO.File.ReadAllText(dataFolder + "test.json");
+            Person obj = JsonConvert.DeserializeObject<Person>(jsonText);
+            Console.WriteLine(obj);
+
+            Person obj2 = new Person
+            {
+                Name = "Mary",
+                Age = 21,
+                FavNums = new List<int> { 10, 20, 30, 40, 50 }
+            };
+            String jsonText2 = JsonConvert.SerializeObject(obj2);
+            System.IO.File.WriteAllText(dataFolder + "test2.json", jsonText2);
+        }
+    }
+}
