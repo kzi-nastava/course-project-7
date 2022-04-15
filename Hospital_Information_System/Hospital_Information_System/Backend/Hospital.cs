53e630db4233d8da53439dca6ece3d038af162f5
magley <60552482+magley@users.noreply.github.com>
2022-04-15 11:41:58 +0200

    Create Room and Hospital with basic serialization
    Create types Room and Hospital and test writing/reading to database
    
    Restructure the project into two directories for the frontend and backend
    respectively.

---
+++ Hospital_Information_System/Hospital_Information_System/Backend/Hospital.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Hospital_Information_System.Backend
+{
+	class Hospital
+	{
+		private static string fnameRooms = "rooms.json";
+		private static JsonSerializerSettings settings = new JsonSerializerSettings{ PreserveReferencesHandling = PreserveReferencesHandling.Objects};
+
+		public List<Room> Rooms { get; set; } = new List<Room>();
+		public void Save(string directory)
+		{
+
+			File.WriteAllText(Path.Combine(directory, fnameRooms), JsonConvert.SerializeObject(Rooms, Formatting.Indented, settings));
+		}
+		public void Load(string directory)
+		{
+			Rooms = JsonConvert.DeserializeObject<List<Room>>(File.ReadAllText(Path.Combine(directory, fnameRooms)), settings);
+		}
+		public override string ToString()
+		{
+			return JsonConvert.SerializeObject(this, Formatting.Indented);
+		}
+	}
+}
---
+++ Hospital_Information_System/Hospital_Information_System/Frontend/CLI/ProgramCLI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Hospital_Information_System.Backend;
+
+namespace Hospital_Information_System.Frontend.CLI
+{
+	internal class ProgramCLI
+	{
+        private static string dataDirectory = Path.Combine("..", "..", "..", "data");
+        static void Main(string[] args)
+        {
+            Hospital hospital = new Hospital();
+
+            // Write from memory into disk.
+
+            //InitHospital(ref hospital);
+            //hospital.Save(dataDirectory);
+
+            // Read from disk into memory.
+
+            hospital.Load(dataDirectory);
+            Console.WriteLine(hospital);
+        }
+        private static void InitHospital(ref Hospital hospital)
+		{
+            hospital.Rooms.Add(new Room(0, Room.RoomType.WAREHOUSE, "Warehouse"));
+
+            const int floorNo = 4;
+            const int bathroomsPerFloor = 7;
+            const int examinationRoomsPerFloor = 5;
+            const int operationRoomsPerFloor = 3;
+            const int recoveryRoomsPerFloor = 4;
+
+            for (int floor = 0; floor < floorNo; floor++)
+            {
+                for (int i = 0; i < bathroomsPerFloor; i++)
+                {
+                    hospital.Rooms.Add(new Room(floor, Room.RoomType.BATHROOM, i));
+                }
+                for (int i = 0; i < examinationRoomsPerFloor; i++)
+                {
+                    hospital.Rooms.Add(new Room(floor, Room.RoomType.EXAMINATION, i));
+                }
+                for (int i = 0; i < operationRoomsPerFloor; i++)
+                {
+                    hospital.Rooms.Add(new Room(floor, Room.RoomType.OPERATION, i));
+                }
+                for (int i = 0; i < recoveryRoomsPerFloor; i++)
+                {
+                    hospital.Rooms.Add(new Room(floor, Room.RoomType.RECOVERY, i));
+                }
+            }
+        }
+    }
+}
