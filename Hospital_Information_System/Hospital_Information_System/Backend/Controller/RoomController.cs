0810b36e7cdcd1b62ba0e60fee7d8f2c9a123b93
magley <magazinlazar2@gmail.com>
2022-04-28 23:03:57 +0200

    refactor: Create RoomController class
    Common logic is extracted to a controller which the model talks to
    
    Aside from that, there are also new extracted methods in the model
    class, for clarity and reducing method size.

---
+++ Hospital_Information_System/Hospital_Information_System/Backend/Controller/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HospitalIS.Backend.Controller
+{
+	internal class RoomController
+	{
+		public enum RoomProperty
+		{
+			NAME,
+			FLOOR,
+			TYPE
+		}
+
+		public static List<RoomProperty> GetRoomProperties()
+		{
+			return Enum.GetValues(typeof(RoomProperty)).Cast<RoomProperty>().ToList();
+		}
+
+		public static void CopyRoom(Room target, Room source, List<RoomProperty> whichProperties)
+		{
+			if (whichProperties.Contains(RoomProperty.NAME)) target.Name = source.Name;
+			if (whichProperties.Contains(RoomProperty.FLOOR)) target.Floor = source.Floor;
+			if (whichProperties.Contains(RoomProperty.TYPE)) target.Type = source.Type;
+		}
+
+		public static List<Room> GetModifiableRooms()
+		{
+			return IS.Instance.Hospital.Rooms.Where(r => !r.Deleted && r.Type != Room.RoomType.WAREHOUSE).ToList();
+		}
+
+		public static List<Room.RoomType> GetModifiableRoomTypes()
+		{
+			return Enum.GetValues(typeof(Room.RoomType)).Cast<Room.RoomType>()
+				.Where(e => e != Room.RoomType.WAREHOUSE)
+				.ToList();
+		}
+	}
+}
