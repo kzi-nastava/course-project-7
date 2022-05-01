ac396d33778ab48d0fc1a34861bbef0a3309f745
magley <60552482+magley@users.noreply.github.com>
2022-05-01 22:41:06 +0200

    feature: Create Renovation class

---
+++ Hospital_Information_System/Hospital_Information_System/Backend/Renovation.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace HospitalIS.Backend
+{
+	internal class Renovation : Entity
+	{
+		[JsonConverter(typeof(Repository.RoomRepository.RoomReferenceConverter))]
+		public Room Room;
+		public DateTime Start;
+		public DateTime End;
+
+		public Renovation(Room room, DateTime start, DateTime end)
+		{
+			Room = room;
+			Start = start;
+			End = end;
+		}
+
+		public Renovation()
+		{
+		}
+
+		public override string ToString()
+		{
+			return $"Renovation(Room={Room.Id} {Room.Name}, Start={Start}, End={End})";
+		}
+	}
+}
