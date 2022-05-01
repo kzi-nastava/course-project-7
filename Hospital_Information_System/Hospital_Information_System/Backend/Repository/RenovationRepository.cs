c691876bc07609bc397b0ee649822daf1fc946e0
magley <60552482+magley@users.noreply.github.com>
2022-05-01 22:51:20 +0200

    feature: Add Renovation repo

---
+++ Hospital_Information_System/Hospital_Information_System/Backend/Repository/RenovationRepository.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace HospitalIS.Backend.Repository
+{
+	internal class RenovationRepository : IRepository<Renovation>
+	{
+        public void Add(Renovation entity)
+        {
+            List<Renovation> Renovations = IS.Instance.Hospital.Renovations;
+
+            entity.Id = Renovations.Count > 0 ? Renovations.Last().Id + 1 : 0;
+            Renovations.Add(entity);
+        }
+
+        public Renovation GetById(int id)
+        {
+            return IS.Instance.Hospital.Renovations.First(e => e.Id == id);
+        }
+
+        public void Load(string fullFilename, JsonSerializerSettings settings)
+        {
+            IS.Instance.Hospital.Renovations = JsonConvert.DeserializeObject<List<Renovation>>(File.ReadAllText(fullFilename), settings);
+        }
+
+        public void Remove(Renovation entity)
+        {
+            entity.Deleted = true;
+        }
+
+        public void Remove(Func<Renovation, bool> condition)
+        {
+            IS.Instance.Hospital.Renovations.ForEach(entity => { if (condition(entity)) Remove(entity); });
+        }
+
+        public void Save(string fullFilename, JsonSerializerSettings settings)
+        {
+            File.WriteAllText(fullFilename, JsonConvert.SerializeObject(IS.Instance.Hospital.Renovations, Formatting.Indented, settings));
+        }
+    }
+}
