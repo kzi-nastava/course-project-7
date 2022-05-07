c991ab588835c4e3aff573e06de1136c17ead61d
magley <60552482+magley@users.noreply.github.com>
2022-05-07 19:05:35 +0200

    feature: Prevent renovations that produce undefined side effects

--- Hospital_Information_System/Hospital_Information_System/Backend/Renovation.cs
+++ Hospital_Information_System/Hospital_Information_System/Backend/Renovation.cs
@@ -11,8 +11,8 @@ namespace HospitalIS.Backend
 		public DateTime End;
 
 		// These are stored directly because they don't exist in the system unti the renovation ends
-		public Room Room1;
-		public Room Room2;
+		public Room Room1 = null;
+		public Room Room2 = null;
 
 		public Renovation(Room room, DateTime start, DateTime end, Room room1 = null, Room room2 = null)
 		{
@@ -21,6 +21,10 @@ namespace HospitalIS.Backend
 			End = end;
 			Room1 = room1;
 			Room2 = room2;
+		}
+
+		public bool IsSplitting() {
+			return Room1 != null && Room2 != null;
 		}
 
 		public Renovation()
--- Hospital_Information_System/Hospital_Information_System/Frontend/CLI/Model/RenovationModel.cs
+++ Hospital_Information_System/Hospital_Information_System/Frontend/CLI/Model/RenovationModel.cs
@@ -9,175 +9,236 @@ using System.Diagnostics;
 
 namespace HospitalIS.Frontend.CLI.Model
 {
-	internal static class RenovationModel
-	{
-		struct Interval
+    internal static class RenovationModel
+    {
+        struct Interval
+        {
+            public DateTime Start;
+            public DateTime End;
+
+            public Interval(DateTime start, DateTime end)
+            {
+                Debug.Assert(start < end);
+                Start = start;
+                End = end;
+            }
+
+            public bool Contains(DateTime dt)
+            {
+                return Start <= dt && dt <= End;
+            }
+
+            public override string ToString()
+            {
+                const string format = "dd.MM.yyyy. HH:mm:ss";
+
+                string start = Start == DateTime.MinValue ? "" : $"{Start.ToString(format)}";
+                string end = End == DateTime.MaxValue ? "" : $"{End.ToString(format)}";
+
+                return $"({start} - {end})";
+            }
+        }
+
+        private const string hintSelectRoom = "Select room for renovation";
+        private const string hintSelectStart = "Select renovation starting time";
+        private const string hintSelectEnd = "Select renovation ending time";
+        private const string infoUnavailable = "Renovation cannot be scheduled during the following times";
+        private const string errDateCutsBusyRoomTime = "The room is in use during that time";
+        private const string errEndDateNotBeforeStart = "Renovation cannot end before its starting timestamp";
+		private const string errStartDateBeforeNow = "Renovation cannot be scheduled into the past!";
+        private const string askWantsToSplit = "Will this room split into 2 new rooms?";
+        private const string hintSelectEquipmentForSplit = "Select equipment to be moved into the first room. The remaining equipment will be moved into the second room";
+        private const string askToRemoveInvalidRenovations = "There exist renovations for this room that will no longer be valid. Would you like to remove them or cancel creating this renovation?";
+
+        public static void NewRenovation(string inputCancelString)
+        {
+            var renovation = InputRenovation(inputCancelString);
+            IS.Instance.RenovationRepo.Add(renovation);
+        }
+
+        private static Renovation InputRenovation(string inputCancelString)
+        {
+            Renovation renovation = new Renovation();
+            Console.WriteLine(hintSelectRoom);
+            renovation.Room = InputRoom(inputCancelString);
+            PrintUnavailableTimeslotsForRenovation(renovation.Room);
+            Console.WriteLine(hintSelectStart);
+            renovation.Start = InputStart(inputCancelString, renovation);
+            Console.WriteLine(hintSelectEnd);
+            renovation.End = InputEnd(inputCancelString, renovation);
+
+			InputSplitRoom(renovation, inputCancelString);
+			InputHandleInvalidRenovationsAfterScheduling(renovation, inputCancelString);
+
+            return renovation;
+        }
+
+		private static void InputHandleInvalidRenovationsAfterScheduling(Renovation renovation, string inputCancelString) 
 		{
-			public DateTime Start;
-			public DateTime End;
+ 			var invalidRenovations = GetInvalidRenovationsAfterScheduling(renovation);
 
-			public Interval(DateTime start, DateTime end)
-			{
-				Debug.Assert(start < end);
-				Start = start;
-				End = end;
-			}
-
-			public bool Contains(DateTime dt)
-			{
-				return Start <= dt && dt <= End;
-			}
-
-			public override string ToString()
-			{
-				const string format = "dd.MM.yyyy. HH:mm:ss";
-				return $"{Start.ToString(format)} - {End.ToString(format)}";
-			}
+            if (invalidRenovations.Count() > 0)
+            {
+                Console.WriteLine(askToRemoveInvalidRenovations);
+                if (EasyInput<bool>.YesNo(inputCancelString))
+                {
+                    foreach (var ren in invalidRenovations)
+                    {
+                        IS.Instance.RenovationRepo.Remove(ren);
+                    }
+                }
+                else
+                {
+                    throw new InputCancelledException();
+                }
+            }
 		}
 
-		private const string hintSelectRoom = "Select room for renovation";
-		private const string hintSelectStart = "Select renovation starting time";
-		private const string hintSelectEnd = "Select renovation ending time";
-		private const string infoUnavailable = "Renovation cannot be scheduled during the following times";
-		private const string errDateCutsBusyRoomTime = "The room is in use during that time";
-		private const string errEndDateNotBeforeStart = "Renovation cannot end before its starting timestamp";
-		private const string askWantsToSplit = "Will this room split into 2 new rooms [Y/N]?";
-		private const string hintSelectEquipmentForSplit = "Select equipment to be moved into the first room. The remaining equipment will be moved into the second room";
-
-		public static void NewRenovation(string inputCancelString)
-		{
-			var renovation = InputRenovation(inputCancelString);
-			IS.Instance.RenovationRepo.Add(renovation);
+		private static void InputSplitRoom(Renovation renovation, string inputCancelString) {
+            Console.WriteLine(askWantsToSplit);
+            if (EasyInput<bool>.YesNo(inputCancelString))
+            {
+                var newRooms = SplitRoom(renovation, inputCancelString);
+                renovation.Room1 = newRooms.Item1;
+                renovation.Room2 = newRooms.Item2;
+            }
 		}
 
-		private static Renovation InputRenovation(string inputCancelString)
-		{
-			Renovation renovation = new Renovation();
-			Console.WriteLine(hintSelectRoom);
-			renovation.Room = InputRoom(inputCancelString);
-			PrintUnavailableTimeslotsForRenovation(renovation.Room);
-			Console.WriteLine(hintSelectStart);
-			renovation.Start = InputStart(inputCancelString, renovation);
-			Console.WriteLine(hintSelectEnd);
-			renovation.End = InputEnd(inputCancelString, renovation);
+        private static Tuple<Room, Room> SplitRoom(Renovation renovation, string inputCancelString)
+        {
+            var roomPropertiesToInput = new List<RoomController.RoomProperty>{
+                RoomController.RoomProperty.TYPE,
+                RoomController.RoomProperty.NAME
+            };
 
-			Console.WriteLine(askWantsToSplit);
-			if (EasyInput<bool>.YesNo(inputCancelString)) {
-				var newRooms = splitRoom(renovation, inputCancelString);
-				renovation.Room1 = newRooms.Item1;
-				renovation.Room2 = newRooms.Item2;
-			}
+            Room r1 = RoomModel.InputRoom(inputCancelString, roomPropertiesToInput);
+            Room r2 = RoomModel.InputRoom(inputCancelString, roomPropertiesToInput);
+            r1.Floor = renovation.Room.Floor;
+            r2.Floor = renovation.Room.Floor;
 
-			return renovation;
-		}
+            Console.WriteLine(hintSelectEquipmentForSplit);
 
-		private static Tuple<Room, Room> splitRoom(Renovation renovation, string inputCancelString) {
-			var roomPropertiesToInput = new List<RoomController.RoomProperty>{
-				RoomController.RoomProperty.TYPE,
-				RoomController.RoomProperty.NAME
-			};
+            var eqForRoom1 = EasyInput<KeyValuePair<Equipment, int>>.SelectMultiple(
+                renovation.Room.Equipment.ToList(),
+                kv => $"{kv.Key.ToString()} ({kv.Value})",
+                inputCancelString
+            );
+            var eqForRoom2 = renovation.Room.Equipment.Except(eqForRoom1);
 
-			Room r1 = RoomModel.InputRoom(inputCancelString, roomPropertiesToInput);
-			Room r2 = RoomModel.InputRoom(inputCancelString, roomPropertiesToInput);
-			r1.Floor = renovation.Room.Floor;
-			r2.Floor = renovation.Room.Floor;
+            Debug.Assert(eqForRoom1.Intersect(eqForRoom2).Count() == 0);
+            Debug.Assert(new HashSet<KeyValuePair<Equipment, int>>(eqForRoom1.Concat(eqForRoom2)).SetEquals(renovation.Room.Equipment));
 
-			Console.WriteLine(hintSelectEquipmentForSplit);
+            foreach (var eqAmount in eqForRoom1)
+            {
+                r1.Equipment.Add(eqAmount.Key, eqAmount.Value);
+            }
 
-			var eqForRoom1 = EasyInput<KeyValuePair<Equipment, int>>.SelectMultiple(
-				renovation.Room.Equipment.ToList(),
-				kv => $"{kv.Key.ToString()} ({kv.Value})",
-				inputCancelString
-			);
-			var eqForRoom2 = renovation.Room.Equipment.Except(eqForRoom1);
+            foreach (var eqAmount in eqForRoom2)
+            {
+                r2.Equipment.Add(eqAmount.Key, eqAmount.Value);
+            }
 
-			Debug.Assert(eqForRoom1.Intersect(eqForRoom2).Count() == 0);
-			Debug.Assert(new HashSet<KeyValuePair<Equipment, int>>(eqForRoom1.Concat(eqForRoom2)).SetEquals(renovation.Room.Equipment));
+            return new Tuple<Room, Room>(r1, r2);
+        }
 
-			foreach (var eqAmount in eqForRoom1) {
-				r1.Equipment.Add(eqAmount.Key, eqAmount.Value);
-			}
+        private static Room InputRoom(string inputCancelString)
+        {
+            return EasyInput<Room>.Select(
+                RoomController.GetModifiableRooms(),
+                inputCancelString
+            );
+        }
 
-			foreach (var eqAmount in eqForRoom2) {
-				r2.Equipment.Add(eqAmount.Key, eqAmount.Value);
-			}
+        private static DateTime InputStart(string inputCancelString, Renovation reference)
+        {
+            Debug.Assert(reference.Room != null);
+            var badDates = GetUnavailableTimeslotsForRenovation(reference.Room);
 
-			return new Tuple<Room, Room>(r1, r2);
-		}
-
-		private static Room InputRoom(string inputCancelString)
-		{
-			return EasyInput<Room>.Select(
-				RoomController.GetModifiableRooms(),
-				inputCancelString
-			);
-		}
-
-		private static DateTime InputStart(string inputCancelString, Renovation reference)
-		{
-			Debug.Assert(reference.Room != null);
-			var badDates = GetUnavailableTimeslotsForRenovation(reference.Room);
-
-			return EasyInput<DateTime>.Get(
-				new List<Func<DateTime, bool>> { dt => badDates.Count(bd => bd.Contains(dt)) == 0 },
-				new[] { errDateCutsBusyRoomTime },
-				inputCancelString
-			);
-		}
-
-		private static DateTime InputEnd(string inputCancelString, Renovation reference)
-		{
-			Debug.Assert(reference.Room != null);
-			var badDates = GetUnavailableTimeslotsForRenovation(reference.Room);
-
-			return EasyInput<DateTime>.Get(
-				new List<Func<DateTime, bool>>
-				{
+            return EasyInput<DateTime>.Get(
+                new List<Func<DateTime, bool>> 
+				{ 
 					dt => badDates.Count(bd => bd.Contains(dt)) == 0,
-					dt => reference.Start <= dt
+					dt => dt < DateTime.Now
 				},
-				new[]
-				{
+                new[] 
+				{ 
 					errDateCutsBusyRoomTime,
-					errEndDateNotBeforeStart
+					errStartDateBeforeNow
 				},
-				inputCancelString
-			);
-		}
+                inputCancelString
+            );
+        }
 
-		private static void PrintUnavailableTimeslotsForRenovation(Room r)
-		{
+        private static DateTime InputEnd(string inputCancelString, Renovation reference)
+        {
+            Debug.Assert(reference.Room != null);
+            var badDates = GetUnavailableTimeslotsForRenovation(reference.Room);
 
-			var unavailableSlotsSorted = GetUnavailableTimeslotsForRenovation(r);
-			unavailableSlotsSorted.Sort((a, b) => (a.Start.CompareTo(b.Start)));
+            return EasyInput<DateTime>.Get(
+                new List<Func<DateTime, bool>>
+                {
+                    dt => badDates.Count(bd => bd.Contains(dt)) == 0,
+                    dt => reference.Start <= dt
+                },
+                new[]
+                {
+                    errDateCutsBusyRoomTime,
+                    errEndDateNotBeforeStart
+                },
+                inputCancelString
+            );
+        }
 
-			Console.WriteLine(infoUnavailable);
-			foreach (var interval in unavailableSlotsSorted)
-			{
-				Console.WriteLine(interval);
-			}
-		}
+        private static void PrintUnavailableTimeslotsForRenovation(Room r)
+        {
+            var unavailableSlotsSorted = GetUnavailableTimeslotsForRenovation(r);
+            unavailableSlotsSorted.Sort((a, b) => (a.Start.CompareTo(b.Start)));
 
-		private static List<Interval> GetUnavailableTimeslotsForRenovation(Room r)
-		{
-			List<Interval> result = new List<Interval>();
+            Console.WriteLine(infoUnavailable);
+            foreach (var interval in unavailableSlotsSorted)
+            {
+                Console.WriteLine(interval);
+            }
+        }
 
-			var relevantRenovations = RenovationController.GetRenovations().Where(ren => ren.Room == r).ToList();
-			foreach (var ren in relevantRenovations)
-			{
-				result.Add(new Interval(ren.Start, ren.End));
-			}
+        private static List<Renovation> GetInvalidRenovationsAfterScheduling(Renovation renovation)
+        {
+            if (renovation.IsSplitting())
+            {
+                return RenovationController.GetRenovations()
+                    .Where(ren => ren.Room == renovation.Room && ren.Start >= renovation.End)
+                    .ToList();
+            }
+            return new List<Renovation>();
+        }
 
-			var relevantAppointments = AppointmentController.GetAppointments().Where(ap => ap.Room == r).ToList();
-			foreach (var ap in relevantAppointments)
-			{
-				// TODO @magley: Utilize "duration" property once it gets implemented into Appointments.
-				DateTime start = ap.ScheduledFor;
-				DateTime end = start.AddMinutes(AppointmentController.LengthOfAppointmentInMinutes);
-				result.Add(new Interval(start, end));
-			}
+        private static List<Interval> GetUnavailableTimeslotsForRenovation(Room r)
+        {
+            List<Interval> result = new List<Interval>();
 
-			return result;
-		}
-	}
+            var relevantRenovations = RenovationController.GetRenovations().Where(ren => ren.Room == r).ToList();
+            foreach (var ren in relevantRenovations)
+            {
+                if (ren.IsSplitting())
+                {
+                    result.Add(new Interval(ren.Start, DateTime.MaxValue));
+                }
+                else
+                {
+                    result.Add(new Interval(ren.Start, ren.End));
+                }
+            }
+
+            var relevantAppointments = AppointmentController.GetAppointments().Where(ap => ap.Room == r).ToList();
+            foreach (var ap in relevantAppointments)
+            {
+                // TODO @magley: Utilize "duration" property once it gets implemented into Appointments.
+                DateTime start = ap.ScheduledFor;
+                DateTime end = start.AddMinutes(AppointmentController.LengthOfAppointmentInMinutes);
+                result.Add(new Interval(start, end));
+            }
+
+            return result;
+        }
+    }
 }
