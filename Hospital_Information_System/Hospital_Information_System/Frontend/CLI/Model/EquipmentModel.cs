8b74171174adfe1d37739affe0e637ee2899166e
Kristina Andrijin <andrijinkristina@gmail.com>
2022-06-04 18:44:55 +0200

    fix: delete only dynamic equipment

--- Hospital_Information_System/Hospital_Information_System/Frontend/CLI/Model/EquipmentModel.cs
+++ Hospital_Information_System/Hospital_Information_System/Frontend/CLI/Model/EquipmentModel.cs
@@ -138,13 +138,20 @@ namespace HospitalIS.Frontend.CLI.Model
 			foreach (KeyValuePair<Equipment, int> entry in oldEquipmentQuantity)
 			{
 				var equipment = entry.Key;
-				Console.Write(equipment + ": ");
-				int currentQuantity = entry.Value;
-				int usedQuantity = GetUsedEquipmentQuantity(inputCancelString, currentQuantity);
-				int newQuantity = currentQuantity - usedQuantity;
-				if (newQuantity != 0)
+				var currentQuantity = entry.Value;
+				if (EquipmentController.IsDynamicEquipment(equipment))
 				{
-					newEquipmentQuantity[equipment] = newQuantity;
+					Console.Write(equipment + ": ");
+					int usedQuantity = GetUsedEquipmentQuantity(inputCancelString, currentQuantity);
+					int newQuantity = currentQuantity - usedQuantity;
+					if (newQuantity != 0)
+					{
+						newEquipmentQuantity[equipment] = newQuantity;
+					}
+				}
+				else
+				{
+					newEquipmentQuantity[equipment] = currentQuantity;
 				}
 			}
 
