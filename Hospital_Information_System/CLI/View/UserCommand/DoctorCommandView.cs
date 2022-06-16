41658296d8230f80c1a9e0022602b1938962ff15
Kristina Andrijin <andrijinkristina@gmail.com>
2022-06-16 23:00:10 +0200

    rename commands

--- Hospital_Information_System/CLI/View/UserCommand/DoctorCommandView.cs
+++ Hospital_Information_System/CLI/View/UserCommand/DoctorCommandView.cs
@@ -11,14 +11,14 @@ namespace HIS.CLI.View.UserCommand
             daysOffRequestView.NotifyDoctor();
             AddCommands(new Dictionary<string, Action>
             {
-                { "app-create", () => appointmentView.CmdCreate() },
-                { "app-read", () => appointmentView.CmdRead() },
-                { "app-update", () => appointmentView.CmdUpdate() },
-                { "app-delete", () => appointmentView.CmdDelete() },
-                { "app-view-start", () => appointmentView.CmdViewAndStartAppointments() },
-                { "dor-read", () => daysOffRequestView.CmdRead() },
-                { "dor-create", () => daysOffRequestView.CmdCreateDaysOffRequest() },
-                { "req-review", () => medicationView.CmdReviewMedicationRequests() },
+                { "appointment-create", () => appointmentView.CmdCreate() },
+                { "appointment-read", () => appointmentView.CmdRead() },
+                { "appointment-update", () => appointmentView.CmdUpdate() },
+                { "appointment-delete", () => appointmentView.CmdDelete() },
+                { "appointment-view-start", () => appointmentView.CmdViewAndStartAppointments() },
+                { "days-off-read", () => daysOffRequestView.CmdRead() },
+                { "days-off-create", () => daysOffRequestView.CmdCreateDaysOffRequest() },
+                { "medication-review", () => medicationView.CmdReviewMedicationRequests() },
             });
         }
     }
